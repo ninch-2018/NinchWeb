c1a6ef990727b8a3bcd2af638ce73ee2da29ff34
aktdaaaa <44158403+aktdaaaa@users.noreply.github.com>
2018-11-08 13:07:08 +0900

    first commit

--- eval.py
+++ eval.py
@@ -2,13 +2,12 @@ import requests
 
 
 def evaluation():
-    # files = {
-    # 'job_id': (None, '20181101-044714-679b'),
-    # 'image_path': (None, '/data/datasets/imagenet/ilsvrc12/pet/Birman_/Birman_12.jpg'),
-    # }
+     files = {
+     'job_id': (None, '20181101-044714-679b'),
+     'image_path': (None, '/data/datasets/imagenet/ilsvrc12/pet/Birman_/Birman_12.jpg'),
+     }
 
-    #response = requests.post('http://localhost:8888/models/images/classification/classify_one.json', files=files)
-    data = {'predictions': [['Birman', 100.0], [
-        'Abyssinian', 0.0], ['Bengal', 0.0], ['Bombay', 0.0]]}
+    response = requests.post('http://localhost:8888/models/images/classification/classify_one.json', files=files)
+    data = response
 
     return data
